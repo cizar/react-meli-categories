2385342516edf26745acafd292a8c9024abd13a1
Cesar Kastli <cesarkastli@gmail.com>
2018-03-26 21:38:09 -0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom'
 import Categories from './containers/CategoriesContainer'
 
 
@@ -6,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Categories />
+        <Route component={Categories} />
       </div>
     );
   }
